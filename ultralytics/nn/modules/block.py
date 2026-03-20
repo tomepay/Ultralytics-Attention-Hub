f06d4ad672adc63fa7afe9b25b31f88a9dea4ea0
tomepay <tomepay@outlook.com>
2026-03-20 11:14:51 +0800

    feat: 适配 YOLOv26 的 CA 模块实现

--- ultralytics/nn/modules/block.py
+++ ultralytics/nn/modules/block.py
@@ -7,11 +7,12 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-from ultralytics.utils.torch_utils import fuse_conv_and_bn, make_divisible
+from ultralytics.utils.torch_utils import fuse_conv_and_bn
 
 from .conv import Conv, DWConv, GhostConv, LightConv, RepConv, autopad
 from .transformer import TransformerBlock
 
+from ultralytics.utils.ops import make_divisible
 
 __all__ = (
     "C1",
